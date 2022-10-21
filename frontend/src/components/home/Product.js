e8678425767bc2f22d22b7bd8d4e1001a17659f9
KwakMirae <alfo9490@naver.com>
2022-10-22 00:08:50 +0900

    feat: add category image and applying it

--- frontend/src/components/home/Product.js
+++ frontend/src/components/home/Product.js
@@ -1,4 +1,7 @@
 import React from "react";
+import COFFEE_BEAN_PACKAGE from "../../img/COFFEE_BEAN_PACKAGE.jpg"
+import MACAROON from "../../img/MACAROON.jpg"
+import SCONE from "../../img/SCONE.jpg"
 
 export function Product(props) {
     const productId = props.productId;
@@ -8,9 +11,20 @@ export function Product(props) {
     const handleAddButtonClicked = e => {
         props.onAddClick(productId);
     };
+    const handleImgByCategory = () => {
+        if( category === "COFFEE_BEAN_PACKAGE") {
+            return <img className="img-fluid" src={COFFEE_BEAN_PACKAGE} alt=""/>
+        }
+        else if( category === "MACAROON"){
+            return <img className="img-fluid" src={MACAROON} alt=""/>
+        }
+        else if( category === "SCONE") {
+            return <img className="img-fluid" src={SCONE} alt=""/>
+        }
+    };
     return (
         <>
-            <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
+            <div className="col-2">{handleImgByCategory()}</div>
             <div className="col">
                 <div className="row text-muted">{category}</div>
                 <div className="row">{productName}</div>
--- frontend/src/components/myorders/OrderItem.js
+++ frontend/src/components/myorders/OrderItem.js
@@ -1,13 +1,28 @@
 import React from "react";
+import COFFEE_BEAN_PACKAGE from "../../img/COFFEE_BEAN_PACKAGE.jpg"
+import MACAROON from "../../img/MACAROON.jpg"
+import SCONE from "../../img/SCONE.jpg"
 
 export function OrderItem(props) {
     const category = props.category;
     const quantity = props.quantity;
     const price = props.price;
 
+    const handleImgByCategory = () => {
+        if( category === "COFFEE_BEAN_PACKAGE") {
+            return <img className="img-fluid" src={COFFEE_BEAN_PACKAGE} alt=""/>
+        }
+        else if( category === "MACAROON"){
+            return <img className="img-fluid" src={MACAROON} alt=""/>
+        }
+        else if( category === "SCONE") {
+            return <img className="img-fluid" src={SCONE} alt=""/>
+        }
+    };
+
     return (
         <>
-            <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
+            <div className="col-2">{handleImgByCategory()}</div>
             <div className="col">
                 <div className="row text-muted">{category}</div>
                 <div className="row">{price}</div>
